7d95cb100640fbeb49b6ca03d35695d06c742749
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-30 22:29:55 -0600

    creating task

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 from fastapi import FastAPI
 from reactpy.backend.fastapi import configure
-from reactpy import component, html
+from reactpy import component, html, use_state
+import reactpy
+from uuid import uuid4
 
 
 @component
 def App():
-    return html.div("Hello World")
+    tasks, setTasks = use_state([])
+    title, setTitle = use_state("")
+    description, setDescription = use_state("")
+
+    @reactpy.event(prevent_default=True)
+    def handle_submit(e):
+        print(title)
+        print(description)
+        new_task = {
+            "title": title,
+            "description": description,
+            "id": uuid4(),
+        }
+        print(new_task)
+
+    return html.div(
+        html.form(
+            {
+                "onsubmit": handle_submit,
+            },
+            html.input({
+                "type": "text",
+                "placeholder": "Write a title",
+                "onchange": lambda e: setTitle(e["target"]["value"]),
+            }),
+            html.textarea({
+                "placeholder": "Write a description",
+                "onchange": lambda e: setDescription(e["target"]["value"]),
+            }),
+            html.button("Create task"),
+        ),
+        html.ul()
+    )
 
 
 app = FastAPI()
